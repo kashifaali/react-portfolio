5a1a82177ceae4dd856293f51b9bfe647b4528da
kashifaali <kashifali4929@gmail.com>
2025-02-09 02:06:32 -0800

    check1

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+<body>
+  <h1>hello</h1>
+</body>
+</html>
+

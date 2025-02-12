1ba03183ca3c01fa4dfecd54f7998265ff0179f9
kashifaali <kashifali4929@gmail.com>
2025-02-12 07:48:14 -0800

    pages update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-portfolio/",
   plugins: [react(),
   tailwindcss(),
   ],
-  base: "/react-portfolio/",
-})
+});
